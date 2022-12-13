42fccd2858d50325992431cd932e4195b7190fd0
Saad Azam <zeroinside4u@gmail.com>
2022-12-13 12:55:14 +0500

    Make comments likeable

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,26 @@
 class LikesController < ApplicationController
   def create
     @like = current_user.likes.new(like_params)
-    p "Killme #{like_params}"
+    # p "Killme #{like_params[:likeable_type]}"
     @like.save
 
-    redirect_to @like.likeable
+    if like_params[:likeable_type] == "Comment"
+      redirect_to @like.likeable.article
+    else
+      redirect_to @like.likeable
+    end
   end
 
   def destroy
     @like = current_user.likes.find(params[:id])
     likeable = @like.likeable
     @like.destroy
-    redirect_to likeable
+
+    if likeable.class.name == "Article"
+      redirect_to likeable
+    else
+      redirect_to likeable.article
+    end
   end
 
   private
