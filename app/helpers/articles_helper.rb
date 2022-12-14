3ec76764b8105226044f10103a6b79694276cdb8
Saad Azam <zeroinside4u@gmail.com>
2022-12-14 15:58:50 +0500

    Remove local variables and SQL queries from article and comment partials Implement helpers to manage likes counter

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,13 @@
 module ArticlesHelper
+  def likeable_likes(likeable)
+    likeable.likes.count
+  end
+
+  def user_likes_likeable?(likeable)
+    current_user.likes.exists?(likeable: likeable)
+  end
+
+  def liked_likeable(likeable)
+    current_user.likes.find_by(likeable: likeable)
+  end
 end
